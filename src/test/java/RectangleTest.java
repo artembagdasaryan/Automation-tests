44e744298e916abda6da4f1bad299d03e150a671
artembagdasaryan <artem.a.bagdasaryan@gmail.com>
2025-04-08 12:28:01 +0300

    for clarifying

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,10 +1,10 @@
-package org.example;
-
+import org.example.Rectangle;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RectangleTest {
